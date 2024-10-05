519c7160f3bf3ab998b8a4dbec3e9d46d6a27067
Fra <fra24eso@gmail.com>
2024-10-05 02:34:52 +0200

    uodate links

--- src/data/linkVideo.js
+++ src/data/linkVideo.js
@@ -3,37 +3,37 @@ export const links = [
         img: "download.jpg",
         title: 'Download from USGS',
         desc: "",
-        video: "https://www.youtube.com/watch?v=gGKe2DtCPR8&list=PLR8J_sq3CKNdfw9ATZvjm_tobxt4bhKdC",
+        video: "https://youtu.be/IWvDe29_sAY?si=zI9IoF8VnD4pBdU2",
     },
     {
         img: "clip.jpg",
         title: 'SCP: Clip multiple bands',
         desc: "",
-        video: "https://www.youtube.com/watch?v=7rlH1pz578o&list=PLR8J_sq3CKNdfw9ATZvjm_tobxt4bhKdC&index=2",
+        video: "https://youtu.be/l8nSEphpBa8?si=6U_7n_IBUMqZW627",
     },
     {
         img: "RT.jpg",
         title: 'SCP: Scaling Reflectance',
         desc: "",
-        video: "https://www.youtube.com/watch?v=Siat-JUgpGc&list=PLR8J_sq3CKNdfw9ATZvjm_tobxt4bhKdC&index=3",
+        video: "https://youtu.be/OUmhqifkmdk?si=l9xJOdj3-3XPgcu_",
     }
     ,
     {
         img: "stack.jpg",
         title: 'Stacking',
         desc: "",
-        video: "https://www.youtube.com/watch?v=ILtHi2RLTkY&list=PLR8J_sq3CKNdfw9ATZvjm_tobxt4bhKdC&index=4",
+        video: "https://youtu.be/EidJg7CQ87U?si=pEnrR74GnmGcLKFO",
     },
     {
         img: "index.jpg",
         title: 'Vegetation indices',
         desc: "",
-        video: "https://www.youtube.com/watch?v=5aiOeA43ZO4&list=PLR8J_sq3CKNdfw9ATZvjm_tobxt4bhKdC&index=5",
+        video: "https://youtu.be/Xekm-WZpklI?si=sZFCP1GXqB3zAKt6",
     },
     {
         img: "altRT.jpg",
         title: 'Alternative Scaling',
         desc: "",
-        video: "https://www.youtube.com/watch?v=G5JRZ5qNuGk&list=PLR8J_sq3CKNdfw9ATZvjm_tobxt4bhKdC&index=6",
+        video: "https://youtu.be/xYUoegMlQlA?si=lxRLt6QVhEBndYEy",
     }
 ]
